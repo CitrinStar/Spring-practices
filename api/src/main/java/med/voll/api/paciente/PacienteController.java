4c92d444cfc6ee0d84b0593e58c54d4e5951d84a
Citrin <lima.stephanielemos@gmail.com>
2024-08-02 14:56:44 -0300

    MVP for medical center web application

---
+++ api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -0,0 +1,45 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public String cadastrar(@RequestBody DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
+       return "Cadastrado com sucesso";
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizar(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        paciente.excluir(id);
+    }
+
+
+}
